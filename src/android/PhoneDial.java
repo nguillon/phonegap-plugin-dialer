124c88f486225ea3fcba8890d5493522784f2fbe
nguillon <nguillon@gmail.com>
2014-03-02 13:59:22 +0100

    test

--- src/android/PhoneDial.java
+++ src/android/PhoneDial.java
@@ -9,18 +9,18 @@ import android.net.Uri;
 import android.content.Intent;
 
 public class PhoneDial extends CordovaPlugin {
-  public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException{	
-if ("call".equals(action)) {
-try{	
-Uri number = Uri.parse("tel:" + args.getString(0));
-Intent callIntent = new Intent(Intent.ACTION_CALL, number);
-this.cordova.getActivity().startActivity(callIntent);
-} catch (Exception e) {
-
-}
-callbackContext.success();
-return true;
-}
-return false;	
-}
+  	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException{	
+		if ("call".equals(action)) {
+			try{	
+				Uri number = Uri.parse("tel:" + args.getString(0));
+				Intent callIntent = new Intent(Intent.ACTION_CALL, number);
+				this.cordova.getActivity().startActivity(callIntent);
+			} catch (Exception e) {
+			
+			}
+			callbackContext.success();
+			return true;
+		}
+		return false;	
+	}
 }
